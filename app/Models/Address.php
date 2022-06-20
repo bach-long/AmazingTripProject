227d057b4179fc7c2c47b80494af327b1040f523
Tran Huu Vinh <tranhuuvinh1109@gmail.com>
2022-06-20 15:21:45 +0700

    component address

---
+++ app/Models/Address.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'address';
+    protected $fillable = ['address_id', 'id_host', 'address_name', 'address_description', 'address_image', 'address_map', 'created_at', 'updated_at'];
+    public $timestamps = true;
+    use HasFactory;
+}
