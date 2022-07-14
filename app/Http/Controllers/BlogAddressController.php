c235d72bf19f425c527a082d362b5c8c53bfdf9b
Thdeathz <buidunga112@gmail.com>
2022-07-15 03:14:18 +0700

    Update BlogAddressController.php

--- app/Http/Controllers/BlogAddressController.php
+++ app/Http/Controllers/BlogAddressController.php
@@ -33,33 +33,14 @@ class BlogAddressController extends Controller
             $blo =  new BlogAddress();
             $blo->id_user = $req->input('id_user');
             $blo->address_id = $req->input('address_id');
-            //$blo->blog_address_title = $req->input('blog_address_title');
+            $blo->blog_address_vote = $req->input('blog_address_vote');
             $blo->blog_address_image = $req->input('blog_address_image');
             $blo->blog_address_content = $req->input('blog_address_content');
 
 
-            $image = $req->blog_address_image;
-            if(!empty($image))
-            {
-                $req->blog_address_image = $image->getClientOriginalName();
-                $image->move('upload/blog_address',$image->getClientOriginalName());
-            }else{
-                $blo->blog_address_image = 'default.jpg';
-            }
-
-
             if($blo->save()){
-                $blog = BlogAddress::where('address_id', $req->address_id)->get();
-                foreach($blog as $i){
-                    $user = User::where('id', $i->id_user)->first();
-                    $i->nickname=$user->nickname;
-                    $i->avatar=$user->avatar;
-                    $i->commentCount = CommentBlogAddress::where('blog_address_id', $i->blog_address_id)->count();
-                    $i->likeCount = ReactionBlogAdress::where('blog_address_id', $i->blog_address_id)->where('reaction', 1)->count();
-                    $i->dislikeCount=ReactionBlogAdress::where('blog_address_id', $i->blog_address_id)->where('reaction', 0)->count();
-                }
                 return response()->json([
-                    'data' => $blog,
+                    'data' => [],
                     'status' => 200,
                     'message' => 'Post blog address successfully'
                 ]);
