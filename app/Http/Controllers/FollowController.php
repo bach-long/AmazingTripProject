8a198b34abdd9ab699e75e0e607d60510bb3eb8e
Thdeathz <buidunga112@gmail.com>
2022-07-20 05:19:50 +0700

    fix get user profile api

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -19,64 +19,69 @@ class FollowController extends Controller
     }
     public function postFollow(Request $req)
     {
-        if($req){
-            $follow = Follow::where('follower', $req->follower)->where('being_follower', $req->being_follower)->orderBy('created_at', 'desc')->first();
-            if($follow)
-            {
-                $follow->follow_status = $req->follow_status;
-                $follow->save();
-                $user = User::select('nickname', 'avatar')->where('id', $follow->being_follower)->first();
-                $follow->nickname = $user->nickname;
-                $follow->avatar = $user->avatar;
+        $follow = Follow::query()
+            ->join('user_travel', 'follow.being_follower', '=', 'user_travel.id')
+            ->select('user_travel.id',
+                'user_travel.nickname',
+                'user_travel.avatar',
+                'follow.follow_status'
+            )
+            ->where('follow.follower', $req->follower)
+            ->where('follow.being_follower', $req->being_follower)
+            ->orderBy('follow.created_at', 'desc')
+            ->first();
+        if ($follow) {
+            $follow->follow_status = $req->follow_status;
+            $follow->save();
+            return response()->json([
+                'data' => $follow,
+                'status' => 200,
+                'message' => 'Follow successfully'
+            ]);
+        } else {
+            $follow = new Follow();
+            $follow->follower = $req->input('follower');
+            $follow->being_follower = $req->input('being_follower');
+            $follow->follow_status = $req->input('follow_status');
+            if ($follow->save()) {
+                $follow = Follow::query()
+                    ->join('user_travel', 'follow.being_follower', '=', 'user_travel.id')
+                    ->select('user_travel.id',
+                        'user_travel.nickname',
+                        'user_travel.avatar',
+                        'follow.follow_status'
+                    )
+                    ->where('follow.follower', $req->follower)
+                    ->where('follow.being_follower', $req->being_follower)
+                    ->orderBy('follow.created_at', 'desc')
+                    ->first();
                 return response()->json([
                     'data' => $follow,
                     'status' => 200,
-                    'message' => 'Follow successfully'
+                    'message' => 'Post follow successfully'
                 ]);
             } else {
-                $follow =  new Follow();
-                $follow->follower = $req->input('follower');
-                $follow->being_follower = $req->input('being_follower');
-                $follow->follow_status = $req->input('follow_status');
-                if($follow->save()){
-                    $follow = Follow::where('follower', $req->follower)->where('being_follower', $req->being_follower)->orderBy('created_at', 'desc')->first();
-                    $user = User::select('nickname', 'avatar')->where('id', $follow->being_follower)->first();
-                    $follow->nickname = $user->nickname;
-                    $follow->avatar = $user->avatar;
-                    return response()->json([
-                        'data' => $follow,
-                        'status' => 200,
-                        'message' => 'Post follow successfully'
-                    ]);
-                }else{
-                    return response()->json([
-                        'data' => $follow,
-                        'status' => 400,
-                        'message' => 'Post follow fail'
-                    ]);
-                }
+                return response()->json([
+                    'data' => $follow,
+                    'status' => 400,
+                    'message' => 'Post follow fail'
+                ]);
             }
-        }else{
-            return response()->json([
-                'status' => 400,
-                'message' => 'Post follow false'
-            ]);
         }
-
-
     }
+
 
     public function deleteFollow($id)
     {
-        if(Follow::find($id)){
-            if(Follow::find($id)->delete()){
+        if (Follow::find($id)) {
+            if (Follow::find($id)->delete()) {
                 $follow = Follow::all();
                 return response()->json([
                     'data' => $follow,
                     'status' => 200,
                     'message' => 'Delete follow successfully'
                 ]);
-            }else{
+            } else {
                 return response()->json([
                     'status' => 400,
                     'message' => 'Delete follow false'
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,42 +7,64 @@ use App\Models\BlogAddress;
 use App\Models\Bookmark;
 use App\Models\CommentBlogAddress;
 use App\Models\Follow;
+use App\Models\GroupMember;
 use App\Models\ReactionBlogAddress;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
 class UserController extends Controller
 {
     public function getProfile($user_id, $current_user_id)
     {
         if ($user_id != 0) {
-            $profile = User::find($user_id);
-            $follow = Follow::where('follower', $current_user_id)->where('being_follower', $user_id)->first();
-//            $list_follow = [];
-//            if(!empty($follow[0]->being_follower))
-//            {
-//                $a = explode(',', $follow[0]->being_follower);
-//                foreach($a as $i)
-//                {
-//                    $user = User::find($i);
-//                    array_push($list_follow, $user);
-//
-//                }
-//            }
-            $blog = BlogAddress::join('address', 'address.address_id', '=', 'blog_address.address_id')
-                ->where('id_user', $user_id)
+            $profile = User::query()
+                ->select('user_travel.id',
+                    'user_travel.nickname',
+                    'user_travel.avatar',
+                    'user_travel.birthday',
+                    'user_travel.address',
+                    'user_travel.created_at',
+                )
+                ->where('user_travel.id', '=', $user_id)
+                ->first();
+            $follow = Follow::query()
+                ->select('follow_status')
+                ->where('follower', '=', $current_user_id)
+                ->where('being_follower', '=', $user_id)
+                ->where('follow_status', '=', '1')
+                ->first();
+            if($follow)
+                $profile->follow_status = $follow->follow_status;
+            else
+                $profile->follow_status = null;
+            $blog = BlogAddress::query()
+                ->join('address', 'blog_address.address_id', '=', 'address.address_id')
+                ->join('user_travel', 'blog_address.id_user', '=', 'user_travel.id')
+                ->select('blog_address.blog_address_id',
+                    'blog_address.blog_address_vote',
+                    'blog_address.blog_address_image',
+                    'blog_address.blog_address_content',
+                    'blog_address.created_at',
+                    'address.address_name',
+                    'address.address_id',
+                    'user_travel.id',
+                    'user_travel.avatar',
+                    'user_travel.nickname'
+                )
+                ->where('blog_address.id_user', '=', $user_id)
                 ->orderBy('blog_address.created_at', 'desc')
                 ->get();
             foreach ($blog as $i) {
-                $i->commentCount = CommentBlogAddress::where('blog_address_id', $i->blog_address_id)->count();
-                $i->likeCount = ReactionBlogAddress::where('blog_address_id', $i->blog_address_id)->where('reaction', 1)->count();
-                $i->dislikeCount = ReactionBlogAddress::where('blog_address_id', $i->blog_address_id)->where('reaction', 0)->count();
+                $i->commentCount = CommentBlogAddress::query()->where('blog_address_id', $i->blog_address_id)->count();
+                $i->likeCount = ReactionBlogAddress::query()->where('blog_address_id', $i->blog_address_id)->where('reaction', 1)->count();
+                $i->dislikeCount = ReactionBlogAddress::query()->where('blog_address_id', $i->blog_address_id)->where('reaction', 0)->count();
             }
             if ($profile) {
                 return response()->json(
                     [
                         'status' => 200,
                         'data' => $profile,
-                        'follow' => $follow,
                         'blog' => $blog
                     ]
                 );
@@ -74,10 +96,18 @@ class UserController extends Controller
                 ->where('follow_status', '=', '1')
                 ->get();
 
+            $group = GroupMember::query()
+                ->join('group', 'group_members.group_id', '=', 'group.group_id')
+                ->select('group.group_name', 'group_members.group_id')
+                ->where('group_members.id_user', '=', $user_id)
+                ->orderBy('group_members.created_at', 'desc')
+                ->get();
+
             return response()->json(
                 [
                     'bookmark' => $bookmark,
                     'follow' => $follow,
+                    'group' => $group,
                     'status' => 200,
                     'message' => 'Get profile successfully'
                 ]
